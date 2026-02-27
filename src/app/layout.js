8b1a94e4ef1b31467df38c87e51658eda40da14f
Raiyan Hasan <raiyanhasan2006@gmail.com>
2026-02-28 04:51:20 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -145,6 +145,13 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en" className={`${spaceGrotesk.variable} ${playfairDisplay.variable}`}>
       <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
