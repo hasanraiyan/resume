21ca0e6204b774a097768111077829bbdce7f477
Raiyan Hasan <raiyanhasan2006@gmail.com>
2026-03-04 16:03:08 +0530

    improved

--- check-api.js
+++
@@ -1,8 +0,0 @@
-const { GoogleGenAI } = require('@google/genai');
-
-const ai = new GoogleGenAI({ apiKey: 'dummy' });
-console.log('Instance properties:', Object.keys(ai));
-if (ai.models) {
-  console.log('ai.models properties:', Object.keys(ai.models));
-}
-console.log('ai.getGenerativeModel type:', typeof ai.getGenerativeModel);
--- check-exports.js
+++
@@ -1,2 +0,0 @@
-import * as agents from './src/lib/agents/index.js';
-console.log('Available exports:', Object.keys(agents));
