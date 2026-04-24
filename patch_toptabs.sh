85f571a43f787bf747b4bc4966c992d38458bc24
hasanraiyan <143262732+hasanraiyan@users.noreply.github.com>
2026-04-24 22:44:15 +0000

    chore: remove patch_toptabs.sh script

--- patch_toptabs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/{option.label && <span className="hidden sm:inline truncate">{option.label}<\/span>}/{option.label \&\& <span className={`truncate ${showLabelOnMobile ? "" : "hidden sm:inline"}`}>{option.label}<\/span>}/' src/components/ui/TopTabs.js
